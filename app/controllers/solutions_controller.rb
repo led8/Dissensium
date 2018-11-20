cc4c6a1d9b2bb0c76116799b381693e3105ce7e8
Arthur Dhuy <a.dhuy@orange.fr>
2018-11-20 10:13:19 +0100

    modif controllers before wbesockets

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,17 +1,15 @@
 class SolutionsController < ApplicationController
 
-  def new
-    @solution = Solution.new
-  end
-
   def create
     @solution = Solution.new(solution_params)
+    @solution.issue = Issue.find(params[:issue_id])
+    @solution.user = current_user
     @solution.save
 
     if @solution.save
-      redirect_to new_issue_path(@issue)
+      # redirect_to new_issue_path(@issue)
     else
-      render :new
+      render 'issues/new'
     end
   end
 
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,17 +1,15 @@
 class VotesController < ApplicationController
 
-  def new
-    @vote = Vote.new
-  end
-
   def create
     @vote = Vote.new(vote_params)
+    @vote.solution = Solution.find(params[:solution_id])
+    @vote.user = current_user
     @vote.save
 
     if @vote.save
-      redirect_to new_issue_path(@issue)
+      # redirect_to new_issue_path(@issue)
     else
-      render :new
+      render 'issues/new'
     end
   end
 
