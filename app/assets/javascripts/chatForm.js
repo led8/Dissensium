ed62b281b01897323c7d0ae219c35e7bb165bd72
Paul Antoine Hocke <hocke.p.a@gmail.com>
2018-11-21 11:36:50 +0100

    chat room template finally done

--- app/assets/javascripts/chatForm.js
+++
@@ -1,5 +0,0 @@
-// $('#create-message input').keydown(function(e) {
-//     if (e.keyCode == 13) {
-//         $('#create-message').submit();
-//     }
-// });
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,5 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+         :recoverable, :rememberable, :validatable
 end
