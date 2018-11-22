96e9e6be9c995c284f53b0e9cbff54575cb787d3
Paul Antoine Hocke <hocke.p.a@gmail.com>
2018-11-22 11:36:13 +0100

    starting

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,21 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :uid
+
+    def connect
+      self.uid = get_connecting_uid
+      logger.add_tags 'ActionCable', uid
+    end
+
+    protected
+
+    # the connection URL to this actioncable/channel must be
+    # domain.tld?uid=the_uid
+    def get_connecting_uid
+      # this is just how I get the user id, you can keep using cookies,
+      # but because this key gets used for redis, I'd
+      # identify_by the user_id instead of the current_user object
+      request.params[:uid]
+    end
   end
 end
--- app/channels/issues_channel.rb
+++ app/channels/issues_channel.rb
@@ -1,5 +1,10 @@
 class IssuesChannel < ApplicationCable::Channel
   def subscribed
     stream_from "issue_#{params[:issue_id]}"
+    ConnectedList.add(uid)
+  end
+
+  def unsubscribed
+    ConnectedList.remove(uid)
   end
 end
