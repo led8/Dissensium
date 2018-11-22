071fc927b813b1e8826f6f04e2adf98dce45eeea
Florent Legeron <legeronflo@hotmail.fr>
2018-11-22 16:41:07 +0100

    start page +alert

--- app/channels/issues_channel.rb
+++ app/channels/issues_channel.rb
@@ -2,4 +2,7 @@ class IssuesChannel < ApplicationCable::Channel
   def subscribed
     stream_from "issue_#{params[:issue_id]}"
   end
+  def appear(data)
+    current_user.appear on: data['appearing_on']
+  end
 end
