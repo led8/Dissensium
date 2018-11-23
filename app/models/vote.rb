6b84fbc88b861e5a6b6118df07f680cb6f57ef81
Florent Legeron <legeronflo@hotmail.fr>
2018-11-23 16:27:28 +0100

    fixing rating

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,6 @@ class Vote < ApplicationRecord
   belongs_to :solution
   belongs_to :user
   validates :rating, presence: true, inclusion: { in: [1, 2, 3, 4, 5] },
-  allow_nil: false, uniqueness: {scope: :user_id}
+  uniqueness: {scope: :user_id}
 
 end
