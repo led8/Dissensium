17d20c8095b04532efb6feb740c435dd6936a2b2
Paul Antoine Hocke <hocke.p.a@gmail.com>
2018-11-27 10:15:58 +0100

    Le commencement de Bobby

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -21,6 +21,33 @@ class Solution < ApplicationRecord
     })
   end
 
+  def bobby
+    unless self.votes.count.zero?
+      byebug
+      grouped_votes = []
+      grouped_votes.push(self.votes.count(5)).push(self.votes.count(4)).push(self.votes.count(3)).push(self.votes.count(2)).push(self.votes.count(1))
+
+      sum = 0
+      sum_of_votes = grouped_votes.map { |x| sum += x }
+
+      majority_vote = sum_of_votes.map { |x| x >= grouped_votes.sum.to_f / 2 }
+
+      majority_vote_index = majority_vote.index(true)
+
+      if majority_vote_index.zero?
+        return " insuffisant"
+      elsif majority_vote_index == 1
+        return " passable"
+      elsif majority_vote_index == 2
+        return " bien"
+      elsif majority_vote_index == 3
+        return " très bien"
+      else
+        return "excellent"
+      end
+    end
+  end
+
   def solution_average_rating
     self.votes.average(:rating) * 20 unless self.votes.average(:rating).nil?
   end
